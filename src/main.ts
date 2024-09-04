2c6db78415e372fa378e30a83d8ea362f37aa80b
Fedor Kostrov <kostrov.f.dev@gmail.com>
2024-09-04 13:06:27 +0300

    port env

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import 'dotenv/config';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT;
+
 async function main() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
-  await app.listen(7000);
+  await app.listen(PORT);
+
+  console.log(`app listens on port ${PORT}`);
 }
 
 main();
